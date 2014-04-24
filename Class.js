51c3e703e2b70f868b700bc5d7264aad714391d3
zhangshaolong <369669902@qq.com>
2014-04-24 23:12:23 +0800

    添加增强类型的类创建

--- Class.js
+++ Class.js
@@ -1,6 +1,62 @@
+/**
+ * @file 面向对象实现继承，子类会自动继承父类的行为
+ * @author：张少龙（zhangshaolong@baidu.com）
+ */
 var Class = function () {
+    /**
+     * 空函数，用来为子类继承方法
+     */
+    var noop = function () {};
+    /**
+     * 获取某个类创建的所有对象，不包括已经销毁的对象
+     * @return {array.instance} 返回类的实例集合
+     */
+    var all = function () {
+        return this.instances;
+    };
+    /**
+     * 查找某个类创建的对象
+     * @private param {number} uuid 对象唯一标识
+     * @return {instance} 返回类的指定实例
+     */
+    var find = function (uuid) {
+        return this.instances[uuid];
+    };
+    /**
+     * 移除某个类创建的对象
+     * @private param {number} uuid 对象唯一标识
+     */
+    var remove = function (uuid) {
+        delete this.instances[uuid];
+    };
+    /**
+     * 对象销毁自身，即切断与创建此对象的类之间的关系，切断引用的父类
+     */
+    var dispose = function () {
+        this.constructor.remove(this._uuid);
+        this.superClass = null;
+    };
+    /**
+     * 唯一标识产生器
+     * @return {number} 返回唯一标识
+     */
+    var uuid = function () {
+        var uuid = 0;
+        return function () {
+            return ++uuid;
+        };
+    };
+    /**
+     * 类实例化对象的方法
+     * @private param {object} options 实例化对象时的配置参数
+     * @return {instance} 返回实例对象
+     */
+    var init = function (options) { // 暂时只支持1个参数
+        return new this(options);
+    };
     var Root;
     return Root = {
+        enhance: false, // 是否是加强模式
         /**
          * @param {methods}，{key} method名字，{value} method 或者 {object}
          * {methods}.{value}为{object}时，{object}结构
@@ -30,9 +86,14 @@ var Class = function () {
             methods = methods || {};
             var F = function () {
                 this.init && this.init.apply(this, arguments);
+                if (Root.enhance) {
+                    F.instances[this._uuid = F.uuid()] = this;
+                }
             };
             if (parent) {
-                F.prototype = new parent();
+                noop.prototype = parent.prototype;
+                F.prototype = new noop();
+                noop.prototype = null;
             }
             for (var name in methods) {
                 if (methods.hasOwnProperty(name)) {
@@ -55,12 +116,19 @@ var Class = function () {
                 F.prototype.superClass = parent;
                 F.prototype.constructor = F;
             }
+            if (Class.enhance) {
+                F.prototype.dispose = dispose;
+            }
             F.create = Root.create;
-            F.init = Root.init;
+            F.init = init;
+            if (Root.enhance) {
+                F.uuid = uuid();
+                F.instances = {};
+                F.all = all;
+                F.find = find;
+                F.remove = remove;
+            }
             return F;
-        },
-        init: function (options) { // 暂时只支持1个参数
-            return new this(options);
         }
     };
 }();
